f34d007624d98d8a7e4847effb8e86a45ffc795e
Vu Dang Khoa <Khoa.vd235354@sis.hust.edu.vn>
2025-12-22 23:29:29 +0700

    Sua nhe

--- backend/app/routes/login.py
+++ backend/app/routes/login.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request, jsonify
 from app.models import Account
+from werkzeug.security import check_password_hash
 
 login_bp = Blueprint("login", __name__)
 
 @login_bp.route("/login", methods=["POST"])
 def login():
-    data = request.get_json(silent=True)
+    data = request.get_json()
 
     if not data:
         return jsonify({"msg": "Dữ liệu không hợp lệ"}), 400
@@ -16,20 +17,16 @@ def login():
     if not username or not password:
         return jsonify({"msg": "Thiếu username hoặc password"}), 400
 
-    # ADMIN
+    # Demo: hệ thống có 1 người dùng duy nhất là admin
     if username == "admin" and password == "123456":
         return jsonify({
-            "msg": "Đăng nhập thành công với quyền quản trị viên",
-            "role": "admin"
-        })
+            "msg": "Đăng nhập thành công"
+        }), 200
 
-    # USER
+    # Tìm kiếm user dựa trên tên đăng nhập (demo)
     acc = Account.query.filter_by(username=username).first()
 
-    if not acc or not acc.check_password(password):
+    if not acc or not check_password_hash(acc.password, password):
         return jsonify({"msg": "Sai username hoặc password"}), 401
 
-    return jsonify({
-        "msg": "Đăng nhập thành công",
-        "role": "user"
-    })
+    return jsonify({"msg": "Đăng nhập thành công",}), 200
